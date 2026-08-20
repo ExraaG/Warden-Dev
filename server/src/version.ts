fe59f2a41a227ad2d40149e838d1f5cf33fa82f3
AlexH <alexh@warden.local>
2026-08-20 21:47:40 +0200

    chore(build): ensure clean build cache and update stamped version

--- server/src/version.ts
+++ server/src/version.ts
@@ -1,6 +1,6 @@
-// Auto-generated hardcoded version: v4-6219a1222
-export const WARDEN_VERSION = 'v4-6219a1222';
+// Auto-generated hardcoded version: v4-2140678b9
+export const WARDEN_VERSION = 'v4-2140678b9';
 export const WARDEN_VERSION_NUMBER = 4;
-export const WARDEN_GIT_HASH = '6219a1222';
+export const WARDEN_GIT_HASH = '2140678b9';
 export const WARDEN_RELEASE_TITLE = "v4 (c5a8da5d4)";
-export const WARDEN_BUILD_TIME = '2026-08-20T17:13:10.798Z';
+export const WARDEN_BUILD_TIME = '2026-08-20T19:47:05.232Z';
